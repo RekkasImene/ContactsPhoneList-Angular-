55a2fb7c353226a4d3d7e746db6f0606b7884e0b
Imene Rekkas <gi_rekkas@esi.dz>
2021-11-09 15:13:42 +0100

    add contact v1

--- src/app/contact-add/contact-add.component.ts
+++ src/app/contact-add/contact-add.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { Contact } from '../contact';
 import { PhoneBookService } from '../phone-book/phone-book.service';
-
 @Component({
   selector: 'app-contact-add',
   templateUrl: './contact-add.component.html',
@@ -10,14 +9,26 @@ import { PhoneBookService } from '../phone-book/phone-book.service';
 export class ContactAddComponent implements OnInit {
   contact!: Contact;
   @Input() name!: String;
+  contacts= [] as Contact[];
 
-  imageW: String = '../../assets/images/warning.png';
-  imageC: String = '../../assets/images/default-icon.jpg';
+  imageW: String = './../assets/images/warning.png';
+  imageC: String = './../assets/images/default-icon.jpg';
+
+  /*@ViewChild('nameCont') nameCont: ElementRef;
+  @ViewChild('phoneCont') phoneCont: ElementRef;
+  this.nameCont.nativeElement.value;
+    this.contact.phoneC = this.phoneCont.nativeElement.value;*/
 
   constructor(private phoneBookService: PhoneBookService) {}
 
   ngOnInit(): void {
     this.contact = this.phoneBookService.getContact(this.name);
   }
-  addContact(name: String, phone: String,image: String) {}
+  addContact(nameCont: String,phoneCont:String) {
+    this.contacts= this.phoneBookService.getContacts();
+    this.contact.nameC = nameCont;
+    this.contact.phoneC = phoneCont;
+    this.contact.imageC = this.imageC;
+    this.contacts.push(this.contact);
+  }
 }
